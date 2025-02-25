737228a4b1fab1c7a0c5822c62e60eafb7eca2e1
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-24 20:19:03 -0500

    card lists added

--- src/components/CardLists.tsx
+++ src/components/CardLists.tsx
@@ -3,36 +3,7 @@ import { Card, Button, Row, Col } from "react-bootstrap";
 import '../styles/style.css';
 import '../styles/Card.css';
 
-interface CardExpProps {
-  card_para: string;
-  img_src: string;
-  card_url: string;
-  card_title: string;
-}
-
-// Accepts `cardData` as a prop from an external source
-interface CardExpListProps {
-  cardData: CardExpProps[];
-}
-
-const CardLists: React.FC<CardExpListProps> = ({ cardData }) => {
-  return (
-    <Row>
-      {cardData.map((card, index) => (
-        <Col key={index} xs={12} sm={12} md={4} className="mt-3">
-          <CardExp 
-            card_title={card.card_title} 
-            card_para={card.card_para} 
-            img_src={card.img_src} 
-            card_url={card.card_url} 
-          />
-        </Col>
-      ))}
-    </Row>
-  );
-};
-
-// CardExp component for rendering a single card
+// Define CardExp First
 const CardExp: React.FC<CardExpProps> = ({ card_para, img_src, card_url, card_title }) => {
   return (
     <Card className="">
@@ -51,6 +22,36 @@ const CardExp: React.FC<CardExpProps> = ({ card_para, img_src, card_url, card_ti
         </div>
       </Card.Body>
     </Card>
+  );
+};
+
+// Interfaces
+interface CardExpProps {
+  card_para: string;
+  img_src: string;
+  card_url: string;
+  card_title: string;
+}
+
+interface CardExpListProps {
+  cardData: CardExpProps[];
+}
+
+// ✅ Then Define CardLists
+const CardLists: React.FC<CardExpListProps> = ({ cardData }) => {
+  return (
+    <Row>
+      {cardData.map((card, index) => (
+        <Col key={index} xs={12} sm={12} md={4} className="mt-3">
+          <CardExp 
+            card_title={card.card_title} 
+            card_para={card.card_para} 
+            img_src={card.img_src} 
+            card_url={card.card_url} 
+          />
+        </Col>
+      ))}
+    </Row>
   );
 };
 
