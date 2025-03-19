cc060c2e20e0f78acaf13c6718fa9ba7c30b9b83
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-19 13:39:13 -0400

    button redirection added

--- src/components/ButtonBar.tsx
+++ src/components/ButtonBar.tsx
@@ -1,35 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/style.css"; // Custom CSS
-import { Container, Button, Col, Row} from 'react-bootstrap';
-
+import { Container, Button, Col, Row } from 'react-bootstrap';
 
 interface ButtonBarProps {
-    button_text: string;
-    link: string;
-  }
+  button_text: string;
+  link: string;
+}
 
+const ButtonBar: React.FC<ButtonBarProps> = ({ button_text, link }) => {
 
-const ButtonBar: React.FC<ButtonBarProps> = ({ button_text , link }) => {
+  const handleClick = () => {
+    // Open link in a new tab (Redirection)
+    window.open(link, "_blank");
 
-return ( 
+    // Create a temporary <a> tag to trigger the download
+    const a = document.createElement("a");
+    a.href = link;
+    a.setAttribute("download", ""); // This forces the download
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+  };
 
-<Container>
-<Row className="rounded background-color-body mt-3 p-2">
-
-<Col className="text-center">
-
-<a href={link} download>
-<Button className="button-custom-color m-1">{ button_text}</Button>
-</a>
-
-
-</Col>
-
-</Row>
-
-</Container>
-
-);
+  return (
+    <Container>
+      <Row className="rounded background-color-body mt-3 p-2">
+        <Col className="text-center">
+          <Button className="button-custom-color m-1" onClick={handleClick}>
+            {button_text}
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default ButtonBar;
