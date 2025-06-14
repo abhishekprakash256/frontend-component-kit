c34f5fe57385c0daf028dbb3dacfb3a100e03831
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-06-14 17:46:01 -0400

    build done

--- dist/components/ArticleImage.js
+++ dist/components/ArticleImage.js
@@ -3,6 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import "../styles/style.css"; // Custom CSS
 import "../styles/ArticleImage.css"; // Custom CSS
 const ArticleImage = ({ image_link }) => {
-    return (_jsx(Container, { children: _jsx(Row, { className: "rounded background-color-body mt-3 p-2", children: _jsx(Col, { className: "text-center", children: _jsx("img", { src: image_link, className: "img-fluid rounded mt-2 mb-2", alt: "Article" }) }) }) }));
+    return (_jsx(Container, { children: _jsx(Row, { className: "rounded background-color-body mt-3 p-2", children: _jsx(Col, { className: "text-center", children: _jsx("img", { src: image_link, className: "img-fluid rounded mt-2 mb-2 article-image-container p-3", alt: "Article" }) }) }) }));
 };
 export default ArticleImage;
