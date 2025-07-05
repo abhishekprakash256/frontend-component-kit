91428ea7a995488ebf7e5bceecea287763f2a50f
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-07-04 23:00:17 -0400

    new component

--- src/components/SkillsBox.tsx
+++ src/components/SkillsBox.tsx
@@ -2,17 +2,35 @@ import React from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import "../styles/style.css"; // Custom CSS
 import "../styles/SkillsBox.css"; // Custom CSS
+import {
+  FaGithub,
+  FaLinkedin,
+  FaTwitter,
+  FaGitlab,
+  FaKaggle,
+  FaMedium
+} from 'react-icons/fa';
 
 
 
 const SkillsBox: React.FC = () => {
   return (
     <Container>
-      <Row className="rounded background-color-body mt-3 p-2">
+      <Row className="rounded background-color-body mt-3 p-3 text-center d-flex justify-content-center">
         <Col className="text-center">
-          <h1 className="bio-font font-color-class heading-responsive-font m-0">
-            this is test
-          </h1>
+            <div className="skills-icons ">
+
+                
+            <FaGithub className="social-icon icon-custom-color mx-2 no-hover-icons" />
+            <FaLinkedin className="social-icon icon-custom-color mx-2 no-hover-icons" />
+            <FaTwitter className="social-icon icon-custom-color mx-2 no-hover-icons" />
+            <FaGitlab className="social-icon icon-custom-color mx-2 no-hover-icons" />
+            <FaKaggle className="social-icon icon-custom-color mx-2 no-hover-icons" />
+            <FaMedium className="social-icon icon-custom-color mx-2 no-hover-icons" />
+          
+          
+          </div>
+
         </Col>
       </Row>
     </Container>
